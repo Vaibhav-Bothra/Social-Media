e26e89f1a5ae02f312c71c4c02b802a446e468c9
Vaibhav <vaibhavbothra2004@gmail.com>
2023-10-22 14:48:22 +0530

    Feat: Added more functionality to user controller.

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,3 +21,47 @@ module.exports.update = function (req, res) {
 module.exports.user = function (req, res) {
   return res.send("<h1>User</h1>");
 };
+
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in");
+};
+
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up");
+};
+
+module.exports.signOut = function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      console.log("Error in logging out of the page:", err);
+      return res.redirect("back");
+    }
+    req.flash("success", "You have logged out successfully!");
+    return res.redirect("/users/signin");
+  });
+};
+
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (!user) {
+      User.create({
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
+      }).then((user) => {
+        console.log(user);
+        return res.redirect("/users/signin");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
+
+module.exports.createSession = function (req, res) {
+  req.flash("success", "Logged in successfully!!");
+  return res.redirect("/");
+};
